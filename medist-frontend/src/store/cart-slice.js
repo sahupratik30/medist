cb598a8d663553bf2a4edf514492093ed50f0664
sahupratik30 <sahupratik30@gmail.com>
2023-01-16 13:05:19 +0530

    added cart data to localstorage

--- medist-frontend/src/store/cart-slice.js
+++ medist-frontend/src/store/cart-slice.js
@@ -1,11 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const items = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const totalQuantity = localStorage.getItem("totalQuantity")
+  ? JSON.parse(localStorage.getItem("totalQuantity"))
+  : 0;
+const totalAmount = localStorage.getItem("totalAmount")
+  ? JSON.parse(localStorage.getItem("totalAmount"))
+  : 0;
+
+function setCartData(items, totalQuantity, totalAmount) {
+  localStorage.setItem("cartItems", JSON.stringify(items.map((item) => item)));
+  localStorage.setItem("totalQuantity", JSON.stringify(totalQuantity));
+  localStorage.setItem("totalAmount", JSON.stringify(totalAmount));
+}
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [],
-    totalQuantity: 0,
-    totalAmount: 0,
+    items: items,
+    totalQuantity,
+    totalAmount,
   },
   reducers: {
     addItemToCart(state, action) {
@@ -28,6 +44,16 @@ const cartSlice = createSlice({
       }
       state.totalQuantity += newItem.quantity;
       state.totalAmount += newItem.quantity * newItem.price;
+      setCartData(state.items, state.totalQuantity, state.totalAmount);
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify(state.items.map((item) => item))
+      );
+      localStorage.setItem(
+        "totalQuantity",
+        JSON.stringify(state.totalQuantity)
+      );
+      localStorage.setItem("totalAmount", JSON.stringify(state.totalAmount));
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
@@ -35,6 +61,7 @@ const cartSlice = createSlice({
       state.items = state.items.filter((item) => item.id !== existingItem.id);
       state.totalQuantity -= existingItem.quantity;
       state.totalAmount -= existingItem.totalPrice;
+      setCartData(state.items, state.totalQuantity, state.totalAmount);
     },
   },
 });
