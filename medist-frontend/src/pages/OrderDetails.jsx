41401d2a0f8329002dbf7456a19714cace7ef4fa
Pratik Sahu <pratiks@logic-square.com>
2023-06-24 08:34:46 +0530

    profile issue fixed in frontend

--- medist-frontend/src/pages/OrderDetails.jsx
+++ medist-frontend/src/pages/OrderDetails.jsx
@@ -1,18 +1,30 @@
+import { useEffect } from "react";
 import OrderItem from "../components/Orders/OrderItem";
 import Card from "../components/UI/Card";
+import { isUserAuthenticated } from "../guards/auth-guard";
+import { useSelector } from "react-redux";
 
 const item = {
   id: 2,
   name: "Sri Sri Tattva Kasahari Cough Syrup 100 ml",
   manufacturer: "Sriveda Sattva Pvt Ltd",
   quantity: 1,
-  price: "57.00",
-  mrp: "60.00",
-  totalPrice: 57,
+  price: "114.00",
+  mrp: "120.00",
+  totalPrice: 120,
   image: "http://127.0.0.1:8000/media/images/lung-care-2_EeYbnTY_bTNOjAM.jpg",
 };
 
 const OrderDetails = () => {
+  const { user } = useSelector((state) => state?.auth || {});
+  const isAuthenticated = isUserAuthenticated();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/signin");
+    }
+  }, [isAuthenticated]);
+
   return (
     <div className="mx-auto my-6 max-w-[996px] px-4">
       <h1 className="mb-10 text-lg font-semibold xs:text-xl s:text-2xl">
@@ -28,7 +40,6 @@ const OrderDetails = () => {
         <div className="flex flex-col gap-4">
           {/* Display the items of the order by mapping over it */}
           <OrderItem item={item} />
-          <OrderItem item={item} />
         </div>
       </Card>
 
@@ -43,20 +54,22 @@ const OrderDetails = () => {
           {/* name */}
           <p className="text-sm font-medium xs:text-base">Pratik Sahu</p>
           {/* street address */}
-          <p className="text-[12px] xs:text-sm">
-            Ward no.1, Pratap Villa, Golei chowk
-          </p>
+          {user?.street_address && (
+            <p className="text-[12px] xs:text-sm">{user?.street_address}</p>
+          )}
           {/* city */}
-          <p className="text-[12px] xs:text-sm">Karanjia</p>
+          <p className="text-[12px] xs:text-sm">{user?.City}</p>
           {/* state and postal code */}
-          <p className="text-[12px] xs:text-sm">Odisha - 757037</p>
+          <p className="text-[12px] xs:text-sm">
+            {user?.state} - {user?.postalcode}
+          </p>
           {/* phone number */}
-          <p className="text-[12px] xs:text-sm">7608998089</p>
+          <p className="text-[12px] xs:text-sm">{user?.phoneNumber}</p>
         </div>
 
         {/* Order delivery status along with date */}
         <p className="text-[12px] font-medium xs:text-sm">
-          Delivered on May 24 2023
+          Ordered on June 24 2023
         </p>
       </Card>
 
@@ -69,15 +82,15 @@ const OrderDetails = () => {
         <div className="mb-6 flex flex-col gap-1">
           <p className="flex items-center justify-between text-[12px] xs:text-sm">
             <span>Payment method</span>
-            <span>UPI</span>
+            <span>Card</span>
           </p>
           <p className="flex items-center justify-between text-[12px] xs:text-sm">
             <span>Total MRP</span>
-            <span>₹60.00</span>
+            <span>₹120.00</span>
           </p>
           <p className="flex items-center justify-between text-[12px] xs:text-sm">
             <span>Total Discount</span>
-            <span className="text-primary">-₹3.00</span>
+            <span className="text-primary">-₹6.00</span>
           </p>
           <p className="flex items-center justify-between text-[12px] xs:text-sm">
             <span>Other Charges</span>
@@ -86,12 +99,13 @@ const OrderDetails = () => {
           <hr />
           <p className="flex items-center justify-between text-[12px] font-semibold xs:text-sm">
             <span>Net Payable</span>
-            <span>₹57.00</span>
+            <span>₹114.00</span>
           </p>
         </div>
 
         <p className="text-[12px] xs:text-sm">
-          You saved <span className="text-primary font-semibold">₹3.00</span> on this order
+          You saved <span className="font-semibold text-primary">₹6.00</span> on
+          this order
         </p>
       </Card>
     </div>
